3b54eeca585be3b293246c49bfcd6d388506b5d9
Omagni <balextsu@gmail.com>
2022-12-13 11:38:45 -0600

    Updated Stocker

--- Grocery Store Game/Assets/Scripts/Shelf.cs
+++ Grocery Store Game/Assets/Scripts/Shelf.cs
@@ -4,7 +4,30 @@ public class Shelf : MonoBehaviour
 {
     // number of items the crate can hold
     [SerializeField] public int stock = 0;
+    [SerializeField] private Sprite image_one;
+    [SerializeField] private Sprite image_two;
+    [SerializeField] private Sprite image_three;
+    [SerializeField] private Sprite image_four;
 
+    SpriteRenderer SR;
+
+    private void Start()
+    {
+        // Get the SpriteRenderer component on the GameObject this script is attached to
+        SR = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        if (stock == 0) // 0
+            SR.sprite = image_one;
+        if (stock > 0 && stock <= 10) // 1-10
+            SR.sprite = image_two;
+        if (stock > 10 && stock <= 15) // 11 - 15
+            SR.sprite = image_three;
+        if (stock > 15) // 16 and above
+            SR.sprite = image_four;
+    }
     public void AddStock(int amount)
     {
         stock += amount;
--- Grocery Store Game/Assets/Scripts/Stocker_Script.cs
+++ Grocery Store Game/Assets/Scripts/Stocker_Script.cs
@@ -1,149 +1,139 @@
 using System.Collections;
 using UnityEngine;
 
-
 /*                      MUST SEE NOTES:
  * Anything you use a IEnumerator function, always use StartCoroutine
  * 
+ * Shelves: Can either have two arrays: GameObject[] and Shelf[]
+ * This makes it that I can simply reference each shelf: ex. (shelves[0].stock)
+ * Or I can have one array of GameObjects. Reference each by (shelfObjects[0].GetComponent<Shelf>().stock)
+ * To save space and optimize, i should use the second version.
  */
-
 
 public class Stocker_Script : MonoBehaviour
 {
-                // Stockers attributes
-    [SerializeField] public float speed = 1.0f;
-    // How many items the stocker can carry/store/grab at once
-    int stockerCarryAmount = 5;
-    // The amount of items stocker is currently holding
-    public int itemsOnHand = 0;
-    public int ActionWaitTime = 2;
+    // STOCKER ATTRIBUTES
+    [SerializeField] public float walkSpeed = 1.0f;
+    int carryAmount = 5; // How many items the stocker can carry/store/grab at once
+    public int itemsOnHand = 0; // The amount of items stocker is currently holding
+    public int actionWaitTime = 2; // how long it takes to complete an action
 
-    // Animations
+    // ANIMATIONS
     public Animator animator;
     private string currentState;
     const string IDLE = "Stocker_Idle";
     const string WALK = "Stocker_Walk";
+    const string IS_STOCKING = "Stocker_Stocking";
 
+    // CHECKS
+    public bool nearTarget = false; // check for near target
+    public bool nearShelf = false; // check for near shelf
+    public bool nearPallet = false; // check for near pallet
+    public bool active = false; // check if Stocker is currently active (working)
+    bool isWaiting = false; // if Stocker is compelting an action
 
-                // Checks
-    // Used for WalkToTarget (function)
-    public bool arrivedAtDestination = false;
-    // Used to check if Stocker near either Shelf or Pallet (functions)
-    public bool nearShelf = false;
-    public bool nearPallet = false;
-    // Check if Stocker is currently active (working)
-    public bool active = false; 
+    // TARGETS
+    public GameObject target; // Stockers current target
+    public Pallet pallet; // ceference to current pallet
 
-    // Stockers current target
-    public GameObject target;
-    public Shelf shelf;
-    public Pallet pallet;
-    // Probably need an array of all shelfs/pallets later down the line.
+    // ARRAYS
+    GameObject[] shelvez; // used for storing game objects of any type into an array
 
-    // To calculate if Stocker is near Shelf/Pallet
+    // INDEXES
+    private int shelfIndex = 0;
+    private int palletIndex = 0;
+
+    // DESTINATION CALCULATIONS
     float distanceFromTarget = 0.0f;
     float distanceFromShelf = 0.0f;
     float distanceFromPallet = 0.0f;
 
     // Stock count references
-    int palletStock = 0;
-    int shelfStock = 0;
-    int stockMax = 20;
-
-    // If Stocker is running a Wait/StartCoroutine - Grabbing/Loading Stock.
-    bool isWaiting = false;
-
+    const int stockMax = 20;
 
     private void Start()
     {
-        shelf = GameObject.FindGameObjectWithTag("Shelf").GetComponent<Shelf>();
         pallet = GameObject.FindGameObjectWithTag("Pallet").GetComponent<Pallet>();
+        shelvez = GameObject.FindGameObjectsWithTag("Shelf"); // Find game objects of type shelf
 
         animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-                //ALWAYS RUNNING FUNCTIONS:
 
+        //reference: 
+        // shelvez[shelfIndex].GetComponent<Shelf>().(use function or variable here)
+
+        //ALWAYS RUNNING FUNCTIONS:
         // If Stocker has a target, walk to it. (Will always walk to target)
         if (target != null)
             WalkToTarget();
-
-        //Always check if near shelf
+        //Always check if near a shelf
         NearShelf();
-        //Always check if near pallet
+        //Always check if near a pallet
         NearPallet();
 
-
-                // CHECKS:
+        // LOGIC:
+        // logic for changing shelves. Simple at the moment. If one shelf reaches max stock, go to next.
+        if (shelvez[shelfIndex].GetComponent<Shelf>().stock == stockMax && shelfIndex < shelvez.Length - 1)
+            NextShelf();
 
         // if stocker is waiting (working/loading/unloading) do not try to do anything else.
         if (isWaiting == false)
-        { 
+        {
+            // if stocker finds a shelf that is not at full capacity, start working
+            if (shelvez[shelfIndex].GetComponent<Shelf>().stock < stockMax & active == false)
+                active = true;
+            // if stock is full: do not start stocking
+            else if (shelvez[shelfIndex].GetComponent<Shelf>().stock == stockMax)
+                active = false;
 
-        // If Shelf is not at stockMax (20), and is not currently stocking: Start stocking
-        if (shelf.stock < stockMax & active == false)
-            active = true;
-        // If stock is full: do not start stocking
-        else if (shelf.stock == stockMax) 
-            active = false;
+            // if Stocker is stocking, and he has no items: Go to Pallet
+            if (active == true & itemsOnHand == 0)
+                TargetPallet();
 
-        // If Stocker is stocking, and he has no items: Go to Pallet
-        if (active == true & itemsOnHand == 0)
-            TargetPallet();
+            // if Stocker is stocking and is near a Pallet and has no items on hand: get stock from Pallet
+            if (active == true & nearPallet == true & itemsOnHand == 0)
+                StartCoroutine(GrabStock(carryAmount));
 
-        // If Stocker is stocking and is near a Pallet and has no items on hand: get stock from Pallet
-        if (active == true & nearPallet == true & itemsOnHand == 0)
-            StartCoroutine( GrabStock(stockerCarryAmount) );
+            // if Stocker is stocking, has all he can carry: Return to Shelf
+            if (active == true & itemsOnHand == carryAmount) {
+                TargetShelf();
+            }
 
-        // If Stocker is stocking, has all he can carry: Return to Shelf
-        if (active == true & itemsOnHand == stockerCarryAmount)
-            TargetShelf();
-
-        // If stocking is stocking and is near Shelf and has items on hand: add items to the Shelf
-        if (active == true & nearShelf == true & itemsOnHand == stockerCarryAmount)
-            StartCoroutine( AddStockToShelf(stockerCarryAmount) );
-
-        }
+            // if stocking is stocking and is near Shelf and has items on hand: add items to the Shelf
+            if (active == true & nearShelf == true & itemsOnHand == carryAmount)
+                StartCoroutine(AddStockToShelf(carryAmount));
+        } // end of isWaiting
 
     }
 
-
     // Target Shelf
-    private void TargetShelf()
+    private void TargetShelf() 
+    { target = shelvez[shelfIndex]; }
+
+    // Target next shelf
+    private void NextShelf()
     {
-        target = GameObject.FindGameObjectWithTag("Shelf");
+        shelfIndex += 1;
+        TargetShelf();
     }
 
     // Target Pallet
-    private void TargetPallet() 
-    {
-        target = GameObject.FindGameObjectWithTag("Pallet");
-    }
-
-
-
+    private void TargetPallet()
+    {target = GameObject.FindGameObjectWithTag("Pallet");}
 
     // Remove stock from Pallets (includes wait time)
-    public IEnumerator GrabStock(int amount) 
+    public IEnumerator GrabStock(int amount)
     {
         // if stocker is not waiting: start wait
         if (isWaiting == false)
             isWaiting = true;
 
-        print("Wait started.");
-
-        yield return new WaitForSeconds(2);
-
-        print("Wait ended");
-
-        // increment stockers items on hand
-        itemsOnHand += amount;
-        // decrement local stock check variable
-        palletStock -= amount;
-        // call the removestock function on Pallet script to decrement its stock value
-        target.GetComponent<Pallet>().RemoveStock(amount);
+        yield return new WaitForSeconds(actionWaitTime); // action wait time
+        itemsOnHand += amount; // increment stockers items on hand
+        pallet.RemoveStock(amount); // decrement current Pallets stock count
         isWaiting = false;
     }
 
@@ -154,43 +144,27 @@ public class Stocker_Script : MonoBehaviour
         if (isWaiting == false)
             isWaiting = true;
 
-        print("Wait started");
-
-        yield return new WaitForSeconds(2);
-
-        print("Wait started");
-
-        // Decrement stockers items on hand
-        itemsOnHand -= amount;
-        // decrement local stock check variable
-        shelfStock += amount;
-        // call the removestock function on Pallet script to decrement its stock value
-        target.GetComponent<Shelf>().AddStock(amount);
+        yield return new WaitForSeconds(actionWaitTime); // action wait time
+        itemsOnHand -= amount; // Decrement stockers items on hand
+        shelvez[shelfIndex].GetComponent<Shelf>().AddStock(amount); // decrement current Shelfs stock count
         isWaiting = false;
     }
 
-
-
-    // Walk to target. Always active. Should probably disable thing if Stocker is in some idle mode.
+    // Walk to target. Always active.
     private void WalkToTarget()
     {
-        // Return if target met.
+        // Cancel function call if Stocker is on target
         if (ArrivedAtDestination() == true)
         {
             ChangeAnimationState(IDLE);
             return;
         }
         else
-        {
             ChangeAnimationState(WALK);
-        }
 
-        //walk to target
-        Vector3 direction = target.transform.position - transform.position;
-        // Keep speed consistent
-        direction.Normalize();
-        // Move the GameObject in the direction of the target at the specified speed
-        transform.Translate(direction * speed * Time.deltaTime);
+        Vector3 direction = target.transform.position - transform.position; // walk to target
+        direction.Normalize(); // keep speed consistent
+        transform.Translate(direction * walkSpeed * Time.deltaTime); // move to target
     }
 
     // Function to determine if stocker has arrived to his destination. Helps control WalkToTarget
@@ -201,44 +175,46 @@ public class Stocker_Script : MonoBehaviour
 
         // if he is 0.5 units away from target, stocker has arrived to destination
         if (distanceFromTarget <= 0.5f)
-            arrivedAtDestination = true;
+            nearTarget = true;
         else
-            arrivedAtDestination = false;
+            nearTarget = false;
 
-        return arrivedAtDestination;
+        return nearTarget; // return bool check
     }
 
-    // Detects if Stocker is near a Shelf
+    // Detects if Stocker is near a Shelf - ALWAYS RUNNING
     public void NearShelf()
     {
-        distanceFromShelf = Vector3.Distance(transform.position, shelf.transform.position);
+        distanceFromShelf = Vector3.Distance(transform.position, shelvez[shelfIndex].GetComponent<Shelf>().transform.position);
 
+        // if he is 0.5 units away from a Shelf, stocker has arrived to destination
         if (distanceFromShelf <= 0.5f)
             nearShelf = true;
         else
             nearShelf = false;
     }
 
-    // Detects if Stocker is near a Pallet
+    // Detects if Stocker is near a Pallet - ALWAYS RUNNING
     public void NearPallet()
     {
         distanceFromPallet = Vector3.Distance(transform.position, pallet.transform.position);
 
+        // if he is 0.5 units away from a Pallet, stocker has arrived to destination
         if (distanceFromPallet <= 0.5f)
             nearPallet = true;
         else
             nearPallet = false;
     }
 
+    // used to change current animation
     void ChangeAnimationState(string newState)
     {
         // stop the same animation from interrupting itself
-        if (currentState == newState) 
+        if (currentState == newState)
             return;
 
         // play the animation
         animator.Play(newState);
-
         // reassign the current state
         currentState = newState;
     }
